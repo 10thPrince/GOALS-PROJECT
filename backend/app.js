64c9e9ddb1e1f4f30c3305fd313723eee9d7f8ab
10thPrince <ntwariprince5050@gmail.com>
2025-07-13 23:20:18 +0200

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
+import goalsRoutes from './routes/goalsRoutes.js'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000
 
-app.get('/api/goals', (req, res)=>{
-    res.status(200).json({message: 'Get Goals!!'})
-})
+app.use('/api/goals', goalsRoutes)
 
 
 app.listen(port, ()=>{
